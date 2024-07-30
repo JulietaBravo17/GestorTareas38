347a158b7ea43d43c456d5ae7330b011c25fd99b
Julieta Ramos <lizethramis17@gmail.com>
2024-07-30 14:13:31 -0600

    Estructura HTML, estilos CSS, inicio de JS

---
+++ src/task.js
@@ -0,0 +1,12 @@
+// Obtener la lista de tareas del almacenamieto del navegador "localStorage"
+let tasks = JSON.parse(localStorage.getItem('tasks')) || [];
+
+// Función para agregar una tarea a la lista de tareas
+export const addTask = (task) => {
+    const newTask = {
+        id: Date.now(),
+        text: task,
+        completed: false,
+    };
+}
+
